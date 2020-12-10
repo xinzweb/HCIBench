3f3d5131a5534dcf07d4957d4e4ffd8d65b6ba1e
cwei44 <weichen621@hotmail.com>
2020-12-10 14:32:49 -0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -230,6 +230,7 @@ echo -e "\e[33mCreating automation part and restoring config files...\e[0m"
 mkdir -p /opt/vmware
 mv $DIR/rvc_rvc/lib/rvc/modules/vsantest/automation /opt
 mv -f $DIR/rvc_rvc/lib/rvc/modules/vsantest/packages/graphites /opt/output/vm-template/
+chmod 755 /opt/output/vm-template/graphites/*
 for subdir in 'conf' 'vdbench-param-files' 'fio-param-files' 'tmp' 'logs'
 do
   if [ -d /tmp/${subdir} ]; then
